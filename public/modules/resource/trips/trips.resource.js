873712a5975283ea0a57f87926c50b32c6315fde
Nicolas Oeschger <noe@exq.ch>
2015-07-15 17:45:11 +0200

    Caching GET requests for trip

--- public/modules/resource/trips/trips.resource.js
+++ public/modules/resource/trips/trips.resource.js
@@ -6,6 +6,8 @@ function TripsResource($resource, REST_URL_PREFIX) {
     return $resource(REST_URL_PREFIX + '/trips/:tripId', {
         tripId: '@id'
     }, {
+        get: {method: 'GET', cache: true},
+        query: {method: 'GET', isArray: true, cache: true},
         update: {method: 'POST'}
     });
 }
