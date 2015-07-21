bb2c443e459fba0743d40e60fe3168288db3d8d9
Nicolas Oeschger <noe@exq.ch>
2015-07-21 18:02:23 +0200

    Added template for login

--- public/modules/content/login/login.controller.js
+++ public/modules/content/login/login.controller.js
@@ -2,9 +2,12 @@
 
 // @ngInject
 function LoginController() {
-    //var vm = this;
+    var vm = this;
 
-
+    vm.login = function () {
+        console.log('Username', vm.username);
+        console.log('Password', vm.password);
+    };
 }
 
 module.exports = LoginController;
