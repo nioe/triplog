29de01ad558aeea304c44d5225ba102baf672c83
Nicolas Oeschger <noe@exq.ch>
2015-07-24 18:00:00 +0200

    Added x-auth-token to allowed headers

--- triplog-server-core/src/main/java/ch/exq/triplog/server/core/boundary/security/CORSFilter.java
+++ triplog-server-core/src/main/java/ch/exq/triplog/server/core/boundary/security/CORSFilter.java
@@ -12,7 +12,7 @@ public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext cres) throws IOException {
         cres.getHeaders().add("Access-Control-Allow-Origin", "*");
-        cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-auth-token");
         cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
         cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
         cres.getHeaders().add("Access-Control-Max-Age", "1209600");
