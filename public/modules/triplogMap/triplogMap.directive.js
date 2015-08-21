44733e04ac6372b4a1e05ae02d9f8b7ee33e0d1a
Nicolas Oeschger <noe@exq.ch>
2015-08-21 15:25:22 +0200

    Calculating covered distance

--- public/modules/triplogMap/triplogMap.directive.js
+++ public/modules/triplogMap/triplogMap.directive.js
@@ -1,6 +1,27 @@
 'use strict';
 
 function TriplogMapDirective() {
+    function calcDistance(latlngs) {
+        var coveredDistance = 0;
+        for (var i = 1; i < latlngs.length; i++) {
+            coveredDistance += latlngs[i - 1].distanceTo(latlngs[i]);
+        }
+
+        if (coveredDistance >= 10000) {
+            // Show distance in km
+            return {
+                distance: Math.round(coveredDistance / 100) / 10,
+                unit: 'km'
+            };
+        } else {
+            // Show distance in m
+            return {
+                distance: Math.round(coveredDistance * 10) / 10,
+                unit: 'm'
+            };
+        }
+    }
+
     return {
         restrict: 'E',
         transclude: true,
@@ -13,11 +34,11 @@ function TriplogMapDirective() {
             L.mapbox.accessToken = 'pk.eyJ1IjoibmVvemVyb29uZSIsImEiOiI5YjRmODE4YzM2OGNhOTNhYmE5NjQwYTkwMzlhYzQ2NyJ9.RdoIup-zRJ-ve3e0cWwiKw';
             var map = L.mapbox.map(element[0], 'mapbox.outdoors');
 
-            // create a red polyline from an arrays of LatLng points
             var polyline = L.polyline(scope.gpsPoints, {color: 'red'}).addTo(map);
-
-            // zoom the map to the polyline
             map.fitBounds(polyline.getBounds());
+
+            var coveredDistance = calcDistance(polyline._latlngs);
+            console.log('coveredDistance', coveredDistance.distance + ' ' + coveredDistance.unit);
         }
     };
 }
