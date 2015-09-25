9e43bee3b5491ca076ed1401cb194dfa661ac6a3
Nicolas Oeschger <noe@exq.ch>
2015-09-25 19:14:38 +0200

    Used new triplog tile for step overview as well

--- public/modules/content/stepOverview/stepOverview.module.js
+++ public/modules/content/stepOverview/stepOverview.module.js
@@ -1,8 +1,10 @@
 'use strict';
 
 module.exports = angular.module('stepOverview', [
+    require('modules/triplogTile').name,
+
     // Template module dependencies (created with browserify-ng-html2js)
-    require('./stepOverview.tpl.html').name,
+    require('./stepOverview.tpl.html').name
 ]);
 
 module.exports.controller('StepOverviewController', require('./stepOverview.controller'));
