3ba8d11cd5f4204babb7635fdfa3f0aaee8cc9c1
Nicolas Oeschger <noe@exq.ch>
2015-10-17 12:51:21 +0200

    Deploy complete client/server EAR instead of WAR

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,15 @@ readonly WILDFLY_URL="${WILDFLY_PROTOCOL}://${WILDFLY_USER}:${WILDFLY_PASSWORD}@
 
 echo ${WILDFLY_URL}
 
-echo "Undeploy old war"
+echo "Undeploy old ear"
 curl -k -S -H "content-Type: application/json" -d "{\"operation\":\"undeploy\", \"address\":[{\"deployment\":\"${ARTIFACT_NAME}\"}]}" --digest ${WILDFLY_URL}
 echo ""
 
-echo "Remove old war"
+echo "Remove old ear"
 curl -k -S -H "content-Type: application/json" -d "{\"operation\":\"remove\", \"address\":[{\"deployment\":\"${ARTIFACT_NAME}\"}]}" --digest ${WILDFLY_URL}
 echo ""
 
-echo "Upload new war"
+echo "Upload new ear"
 bytes_value=`curl -k -F "file=@${ARTIFACT_PATH}/${ARTIFACT_NAME}" --digest ${WILDFLY_URL}/add-content | perl -pe 's/^.*"BYTES_VALUE"\s*:\s*"(.*)".*$/$1/'`
 echo $bytes_value
 
@@ -19,7 +19,7 @@ json_string_start='{"content":[{"hash": {"BYTES_VALUE" : "'
 json_string_end="\"}}], \"address\": [{\"deployment\":\"${ARTIFACT_NAME}\"}], \"operation\":\"add\", \"enabled\":\"true\"}"
 json_string="$json_string_start$bytes_value$json_string_end"
 
-echo "Deploy new war"
+echo "Deploy new ear"
 result=`curl -k -S -H "Content-Type: application/json" -d "$json_string" --digest ${WILDFLY_URL} | perl -pe 's/^.*"outcome"\s*:\s*"(.*)".*$/$1/'`
 echo $result
 
