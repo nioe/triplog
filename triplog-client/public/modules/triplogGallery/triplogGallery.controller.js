140f382d835fb74ab64216243a2ff0a310dd1f2f
Nicolas Oeschger <noe@exq.ch>
2015-11-19 07:05:36 +0100

    Sort pictures in gallery by captureDate

--- triplog-client/public/modules/triplogGallery/triplogGallery.controller.js
+++ triplog-client/public/modules/triplogGallery/triplogGallery.controller.js
@@ -5,9 +5,15 @@ function TriplogGalleryController(Lightbox) {
 
     var vm = this;
 
+    vm.pictures.sort(byCaptureDate);
+
     vm.showFullPicture = function (index) {
         Lightbox.openModal(vm.pictures, index);
     };
+
+    function byCaptureDate(picture1, picture2) {
+        return picture1.captureDate.localeCompare(picture2.captureDate);
+    }
 }
 
 module.exports = TriplogGalleryController;
