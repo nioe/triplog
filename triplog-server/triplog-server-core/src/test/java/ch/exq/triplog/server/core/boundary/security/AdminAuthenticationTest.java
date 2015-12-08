d31c30d082c641c2879d3af78157ba433efdc915
Nicolas Oeschger <noe@exq.ch>
2015-12-08 07:30:50 +0100

    Improved AdminAuthenticationTest

--- triplog-server/triplog-server-core/src/test/java/ch/exq/triplog/server/core/boundary/security/AdminAuthenticationTest.java
+++ triplog-server/triplog-server-core/src/test/java/ch/exq/triplog/server/core/boundary/security/AdminAuthenticationTest.java
@@ -1,46 +1,35 @@
 package ch.exq.triplog.server.core.boundary.security;
 
-import ch.exq.triplog.server.util.config.Hardcoded;
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static ch.exq.triplog.server.util.config.Hardcoded.configuration;
+import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * Created by Nicolas Oeschger <noe@exq.ch> on 27.03.2014.
- */
 public class AdminAuthenticationTest {
     private static final String CORRECT_USER = "admin";
     private static final String CORRECT_PASSWORD = "password";
     private static final String WRONG_USER = "foo";
     private static final String WRONG_PASSWORD = "bar";
 
-    private AdminAuthentication toTest;
+    private AdminAuthentication adminAuthentication = new AdminAuthentication(configuration(CORRECT_USER), configuration(CORRECT_PASSWORD));;
 
-    @Before
-    public void setUp() {
-        toTest = new AdminAuthentication();
-        Hardcoded.enableDefaults(toTest);
+    @Test
+    public void should_return_true_if_correct_user_and_password_are_given() throws Exception {
+        assertThat(adminAuthentication.isValid(CORRECT_USER, CORRECT_PASSWORD)).isTrue();
     }
 
     @Test
-    public void testIsValidCorrect() throws Exception {
-        assertTrue(toTest.isValid(CORRECT_USER, CORRECT_PASSWORD));
+    public void should_return_false_if_wrong_user_is_given() throws Exception {
+        assertThat(adminAuthentication.isValid(WRONG_USER, CORRECT_PASSWORD)).isFalse();
     }
 
     @Test
-    public void testIsValidWrongUser() throws Exception {
-        assertFalse(toTest.isValid(WRONG_USER, CORRECT_PASSWORD));
+    public void should_return_false_if_wrong_password_is_given() throws Exception {
+        assertThat(adminAuthentication.isValid(CORRECT_USER, WRONG_PASSWORD)).isFalse();
     }
 
     @Test
-    public void testIsValidWrongPassword() throws Exception {
-        assertFalse(toTest.isValid(CORRECT_USER, WRONG_PASSWORD));
-    }
-
-    @Test
-    public void testIsValidWrongUserAndPassowrd() throws Exception {
-        assertFalse(toTest.isValid(WRONG_USER, WRONG_PASSWORD));
+    public void should_return_false_if_wrong_user_and_password_are_given() throws Exception {
+        assertThat(adminAuthentication.isValid(WRONG_USER, WRONG_PASSWORD)).isFalse();
     }
 }
