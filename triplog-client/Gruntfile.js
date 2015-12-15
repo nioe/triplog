a34c76a85557ad0876887fc986a1e8b7e8fd27e9
Nicolas Oeschger <noe@exq.ch>
2015-12-15 18:57:01 +0100

    Disabled mangle function of uglify to get rid of unknownProvider error

--- triplog-client/Gruntfile.js
+++ triplog-client/Gruntfile.js
@@ -168,6 +168,9 @@ module.exports = function (grunt) {
         },
 
         uglify: {
+            options: {
+                mangle: false
+            },
             all: {
                 options: {
                     sourceMap: false,
