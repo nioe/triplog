0f8acdac674dc3e04dad725409288b08c7a55bee
Nicolas Oeschger <noe@exq.ch>
2016-02-25 17:32:40 +0100

    Added run method for sync service

--- triplog-client/public/modules/resource/sync/sync.module.js
+++ triplog-client/public/modules/resource/sync/sync.module.js
@@ -3,9 +3,22 @@
 module.exports = angular.module('sync', [
     require('modules/tripsResource').name,
     require('modules/stepsResource').name,
-    require('modules/processQueue').name
+    require('modules/processQueue').name,
+    require('modules/config').name
 ]);
 
 module.exports.factory('SyncService', require('./sync.service'));
 
 module.exports.constant('ITEMS_SYNCED_EVENT', 'items-synced');
+
+module.exports.run(['$rootScope', '$interval', 'SyncService', 'SYNC_INTERVAL', function ($rootScope, $interval, SyncService, SYNC_INTERVAL) {
+    // Start sync service once client got online again
+    $rootScope.$on('isOnline', function (event, isOnline) {
+        if (isOnline) {
+            SyncService.sync();
+        }
+    });
+
+    // Start sync service frequently
+    $interval(SyncService.sync, SYNC_INTERVAL);
+}]);
--- triplog-client/public/modules/triplogApp.js
+++ triplog-client/public/modules/triplogApp.js
@@ -15,7 +15,8 @@ var triplogApp = angular.module('triplogApp', [
     require('modules/loginResource').name,
     require('modules/error').name,
     require('modules/alert').name,
-    require('modules/config').name
+    require('modules/config').name,
+    require('modules/sync').name
 ]);
 
 triplogApp.config(function ($stateProvider, $urlRouterProvider, AnalyticsProvider, GOOGLE_ANALYTICS_TRACKING_CODE) {
@@ -52,7 +53,7 @@ triplogApp.config(function ($stateProvider, $urlRouterProvider, AnalyticsProvide
                 transitionSelectorClass: 'content-transition'
             },
             resolve: {
-                checkLoginBefore: function(LoginService) {
+                checkLoginBefore: function (LoginService) {
                     return LoginService.checkPresentToken();
                 },
                 trips: function (checkLoginBefore, TripsService) {
@@ -114,91 +115,91 @@ triplogApp.config(function ($stateProvider, $urlRouterProvider, AnalyticsProvide
 });
 
 triplogApp.run(['$rootScope', '$state', '$stateParams', '$timeout', '$document', '$window', 'localStorageService', 'Analytics', 'AlertService', 'ENV',
-        function ($rootScope, $state, $stateParams, $timeout, $document, $window, localStorageService, Analytics, AlertService, ENV) {
+    function ($rootScope, $state, $stateParams, $timeout, $document, $window, localStorageService, Analytics, AlertService, ENV) {
 
-            $rootScope.$state = $state;
-            $rootScope.$stateParams = $stateParams;
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
 
-            $rootScope.alerts = [];
+        $rootScope.alerts = [];
 
-            $rootScope.isOnline = $window.navigator.onLine || ENV === 'local';
+        $rootScope.isOnline = $window.navigator.onLine || ENV === 'local';
 
-            if ($rootScope.isOnline && ENV !== 'local') {
-                Analytics.offline(false);
-                Analytics.createAnalyticsScriptTag();
-                $rootScope.scriptTagCreated = true;
+        if ($rootScope.isOnline && ENV !== 'local') {
+            Analytics.offline(false);
+            Analytics.createAnalyticsScriptTag();
+            $rootScope.scriptTagCreated = true;
+        }
+
+        $window.addEventListener('offline', function () {
+            $rootScope.$apply(function () {
+                if (ENV !== 'local') {
+                    $rootScope.isOnline = false;
+                    Analytics.offline(true);
+                    $rootScope.$broadcast('isOnline', false);
+                }
+            });
+        }, false);
+
+        $window.addEventListener('online', function () {
+            $rootScope.$apply(function () {
+                if (ENV !== 'local') {
+                    $rootScope.isOnline = true;
+                    Analytics.offline(false);
+                    $rootScope.$broadcast('isOnline', true);
+
+                    if (!$rootScope.scriptTagCreated) {
+                        Analytics.createAnalyticsScriptTag();
+                        $rootScope.scriptTagCreated = true;
+                    }
+                }
+            });
+        }, false);
+
+        $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState) {
+            // Special Animations
+            if (toState.name === 'welcome' || fromState.name === 'welcome') {
+                $rootScope.animationClass = 'welcome-animation';
+            } else {
+                $rootScope.animationClass = undefined;
+            }
+        });
+
+        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            // Scroll to top of content container after state change
+            var contentContainer = $document[0].querySelector('.content-container');
+            if (contentContainer) {
+                contentContainer.scrollTop = 0;
             }
 
-            $window.addEventListener('offline', function () {
-                $rootScope.$apply(function () {
-                    if (ENV !== 'local') {
-                        $rootScope.isOnline = false;
-                        Analytics.offline(true);
-                        $rootScope.$broadcast('isOnline', false);
-                    }
-                });
-            }, false);
+            // Save current state to local storage
+            fromParams.referrerState = undefined;
+            toParams.referrerState = {state: fromState, params: fromParams};
 
-            $window.addEventListener('online', function () {
-                $rootScope.$apply(function () {
-                    if (ENV !== 'local') {
-                        $rootScope.isOnline = true;
-                        Analytics.offline(false);
-                        $rootScope.$broadcast('isOnline', true);
-
-                        if (!$rootScope.scriptTagCreated) {
-                            Analytics.createAnalyticsScriptTag();
-                            $rootScope.scriptTagCreated = true;
-                        }
-                    }
-                });
-            }, false);
-
-            $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState) {
-                // Special Animations
-                if (toState.name === 'welcome' || fromState.name === 'welcome') {
-                    $rootScope.animationClass = 'welcome-animation';
-                } else {
-                    $rootScope.animationClass = undefined;
-                }
+            localStorageService.set('lastState', {
+                state: toState,
+                params: toParams
             });
+        });
 
-            $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-                // Scroll to top of content container after state change
-                var contentContainer = $document[0].querySelector('.content-container');
-                if (contentContainer) {
-                    contentContainer.scrollTop = 0;
-                }
+        $rootScope.$on('$viewContentLoaded', function () {
+            $timeout(function () {
+                $rootScope.hideStartScreen = true;
+            }, 0);
+        });
 
-                // Save current state to local storage
-                fromParams.referrerState = undefined;
-                toParams.referrerState = {state: fromState, params: fromParams};
+        $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+            console.error('State Change Error:', error);
 
-                localStorageService.set('lastState', {
-                    state: toState,
-                    params: toParams
-                });
-            });
-
-            $rootScope.$on('$viewContentLoaded', function () {
-                $timeout(function () {
-                    $rootScope.hideStartScreen = true;
-                }, 0);
-            });
-
-            $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-                console.error('State Change Error:', error);
-
-                switch (error.status) {
-                    case 0:
-                        AlertService.info('Oops, there was a problem loading the data. Please try again later.');
-                        break;
-                    case 404:
-                        $state.go('content.notFound', {}, {reset: true});
-                        break;
-                    default:
-                        AlertService.info(error.data);
-                }
-            });
-        }]
+            switch (error.status) {
+                case 0:
+                    AlertService.info('Oops, there was a problem loading the data. Please try again later.');
+                    break;
+                case 404:
+                    $state.go('content.notFound', {}, {reset: true});
+                    break;
+                default:
+                    AlertService.info(error.data);
+            }
+        });
+    }]
 );
