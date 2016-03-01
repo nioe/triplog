e0a7272233392aebc0b4a71110dd4cc8912d284d
Nicolas Oeschger <noe@exq.ch>
2016-03-01 17:37:02 +0100

    Loading spinner is now shown for route changes instead of http requests

--- triplog-client/public/modules/loadingSpinner/loadingSpinner.controller.js
+++ triplog-client/public/modules/loadingSpinner/loadingSpinner.controller.js
@@ -1,13 +1,28 @@
 'use strict';
 
 // @ngInject
-function LoadingSpinnerController($http) {
+function LoadingSpinnerController($rootScope) {
 
     var vm = this;
+    vm.isShown = false;
 
-    vm.isShown = function () {
-        return $http.pendingRequests.length !== 0;
-    };
+    $rootScope.$on('', function () {
+       vm.isShown = true;
+    });
+
+    $rootScope.$on('$stateChangeStart', showSpinner);
+    $rootScope.$on('$stateNotFound', hideSpinner);
+    $rootScope.$on('$stateChangeSuccess', hideSpinner);
+    $rootScope.$on('$stateChangeError', hideSpinner);
+
+
+    function showSpinner() {
+        vm.isShown = true;
+    }
+
+    function hideSpinner() {
+        vm.isShown = false;
+    }
 }
 
 module.exports = LoadingSpinnerController;
